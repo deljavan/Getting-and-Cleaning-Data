948d7c33933f382140d562d5b5500676f1ccfb22
deljavan <deljavan@yahoo.com>
2015-10-23 23:15:48 -0400

    second commit

--- run_analysis.R
+++ run_analysis.R
@@ -2,18 +2,18 @@
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl,destfile="./GCDCP-Dataset.zip")
 unzip(zipfile="./GCDCP-Dataset.zip")
-path_rf <- file.path("./" , "UCI HAR Dataset")
-files<-list.files(path_rf, recursive=TRUE)
+filepath <- file.path("." , "UCI HAR Dataset")
+files<-list.files(filepath, recursive=TRUE)
 
 ##Read the Activity files
-dataActivityTest  <- read.table(file.path(path_rf, "test" , "Y_test.txt" ),header = FALSE)
-dataActivityTrain <- read.table(file.path(path_rf, "train", "Y_train.txt"),header = FALSE)
-
-dataSubjectTrain <- read.table(file.path(path_rf, "train", "subject_train.txt"),header = FALSE)
-dataSubjectTest  <- read.table(file.path(path_rf, "test" , "subject_test.txt"),header = FALSE)
-
-dataFeaturesTest  <- read.table(file.path(path_rf, "test" , "X_test.txt" ),header = FALSE)
-dataFeaturesTrain <- read.table(file.path(path_rf, "train", "X_train.txt"),header = FALSE)
+dataActivityTest  <- read.table(file.path(filepath, "test" , "Y_test.txt" ),header = FALSE)
+dataActivityTrain <- read.table(file.path(filepath, "train", "Y_train.txt"),header = FALSE)
+##Read the Subject files
+dataSubjectTrain <- read.table(file.path(filepath, "train", "subject_train.txt"),header = FALSE)
+dataSubjectTest  <- read.table(file.path(filepath, "test" , "subject_test.txt"),header = FALSE)
+##Read Fearures files
+dataFeaturesTest  <- read.table(file.path(filepath, "test" , "X_test.txt" ),header = FALSE)
+dataFeaturesTrain <- read.table(file.path(filepath, "train", "X_train.txt"),header = FALSE)
 
 ## Concatenate the data tables by rows
 dataSubject <- rbind(dataSubjectTrain, dataSubjectTest)
@@ -23,7 +23,7 @@ dataFeatures<- rbind(dataFeaturesTrain, dataFeaturesTest)
 ##set names to variables and merge data frame
 names(dataSubject)<-c("subject")
 names(dataActivity)<- c("activity")
-dataFeaturesNames <- read.table(file.path(path_rf, "features.txt"),head=FALSE)
+dataFeaturesNames <- read.table(file.path(filepath, "features.txt"),head=FALSE)
 names(dataFeatures)<- dataFeaturesNames$V2
 
 dataCombine <- cbind(dataSubject, dataActivity)
@@ -34,8 +34,9 @@ subdataFeaturesNames<-dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", dataFeat
 selectedNames<-c(as.character(subdataFeaturesNames), "subject", "activity" )
 Data<-subset(Data,select=selectedNames)
 
-activityLabels <- read.table(file.path(path_rf, "activity_labels.txt"),header = FALSE)
+activityLabels <- read.table(file.path(filepath, "activity_labels.txt"),header = FALSE)
 
+##Names of Feteatures will labelled using descriptive variable names.
 names(Data)<-gsub("^t", "time", names(Data))
 names(Data)<-gsub("^f", "frequency", names(Data))
 names(Data)<-gsub("Acc", "Accelerometer", names(Data))
